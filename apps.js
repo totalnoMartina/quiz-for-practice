1f76ec4ff0415a1e90b8392f11dfa7b366af8b7e
Martina Grganic <totalnobzvz@yahoo.com>
2021-09-28 15:10:09 +0000

    Add more code to js files

--- apps.js
+++ apps.js
@@ -3,9 +3,9 @@
 
 function populate() {
     if(quiz.isEnded()) {
-        // show scores
         showScores();
-    } else {
+    } 
+    else {
         // show questions
         var element = document.getElementById("questions");
         element.innerHTML = quiz.getQuestionIndex().text;
@@ -15,10 +15,30 @@ function populate() {
         for(var i = 0; i< choices.length; i++) {
             var element = document.getElementById("choice" + i);
             element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
         }
-        populate()
-}
+        showProgress();
+};
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
 
+function showProgress() {
+    var currentQuestion = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestion + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    var gameOverHtml = "<h1>Result</h1>";
+    gameOverHtml += "<h2 id='score'> Your score is " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHtml;
+};
 
 var questions = [
     new Question("Which berry is considered to be healthier to take frozen than raw?", 
@@ -28,28 +48,6 @@ var questions = [
     new Question("Eating this fruit daily can improve your vision. Which one is not one of them?", ["Cantaloupe", "Banana", "Avocado", "Blackberry"], "Banana")]
 
 var quiz = new Quiz(questions);
-
-function showProgress() {
-    var currentQuestionNr = quiz.questionIndex + 1;
-    var element = document.getElementById("progress");
-    element.innerHTML = "Question " + currentQuestionNr + " of" + quiz.questions.length;
-}
-
-function guess(id, guess) {
-    var button = document.getElementById('btn');
-    button.onclick = function() {
-        quiz.guess(guess);
-        populate();
-    }
-
-}
-
-function showScores() {
-    var gameOverHtml = "<h1>Result</h1>";
-    gameOverHtml = "<h2 id='score'>Your score is " + quiz.score + "</h2>";
-    var element = document.getElementById("quiz");
-    element.innerHTML = gameOverHtml;
-}
-
+populate()
 
     
--- questions.js
+++ questions.js
@@ -4,18 +4,7 @@ function Question(text, choices, answer) {
     this.answer = answer;
 }
 
-Question.prototype.correctAnswer = function(choices) {
+Question.prototype.correctAnswer = function(choice) {
     return choice === this.answer;
 }
 
-var question = [
-    new Question("Which berry is considered to be healthier to take frozen than raw?", 
-    ["Strawberry", "Blueberry", "Elderberry", "Raspberry"], "Blueberry"),
-    new Question("Which fruit is part of rose family?", ["Pear", "Apricot", "Peach", "Apple"], "Apple"),
-    new Question("Which one of these is actually a berry?", ["Mango", "Cherry", "Eggplant", "Strawberry"], "Eggplant"),
-    new Question("Eating this fruit daily can improve your vision. Which one is not one of them?", ["Cantaloupe", "Banana", "Avocado", "Blackberry"], "Banana"),
-    ]
-
-var quiz = new Quiz(questions);
-
-populate(); 
